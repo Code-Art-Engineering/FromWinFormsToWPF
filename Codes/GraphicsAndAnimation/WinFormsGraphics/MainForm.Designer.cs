ff2974bea003bb3be5264ea89c0e7c9cc634c5e1
Code Artist <codearteng@gmail.com>
2024-10-26 16:50:33 +0800

    Added project GraphicsAndAnimation

--- Codes/GraphicsAndAnimation/WinFormsGraphics/MainForm.Designer.cs
+++ Codes/GraphicsAndAnimation/WinFormsGraphics/MainForm.Designer.cs
@@ -173,7 +173,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1192, 773);
+            this.ClientSize = new System.Drawing.Size(1037, 375);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
